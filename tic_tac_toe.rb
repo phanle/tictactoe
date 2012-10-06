4ed543d85b5b407a6bf8dd08f7ca04dc893aa410
Phan L <phan.anh.le@gmail.com>
2012-10-06 12:07:35 -0700

    Add position translator

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,12 +1,10 @@
 class TicTacToe
   attr_reader :board
-
-  X_POSITION_MAPPING = {'a' => 0,
-                        'b' => 1,
-                        'c' => 2}
+  attr_reader :position_translator
 
   def initialize
     @board = [[], [], []]
+    @position_translator = PositionTranslator.new
   end
 
   def winner
@@ -14,8 +12,14 @@ class TicTacToe
   end
 
   def place(symbol, position)
-    x,y = position[0], position[1].to_i
-    puts x, y
-    @board[X_POSITION_MAPPING[x]][y] = symbol
+    x, y = position_translator.translate(position)
+    @board[x][y] = symbol
+  end
+end
+
+class PositionTranslator
+  X_POSITION_MAPPING = {'a' => 0, 'b' => 1, 'c' => 2}
+  def translate(friendly_position)
+    x,y = X_POSITION_MAPPING[friendly_position[0]], (friendly_position[1].to_i-1)
   end
 end
--- tic_tac_toe_spec.rb
+++ tic_tac_toe_spec.rb
@@ -11,12 +11,18 @@ describe TicTacToe do
 
   context "when playing a game" do
     it 'returns the correct winner' do
-      debugger
       @game.place('x', 'a1')
       @game.place('o', 'b1')
       @game.place('x', 'a2')
       @game.place('o', 'b2')
       @game.place('x', 'a3').should == 'x'
     end
+  end
+end
+
+
+describe PositionTranslator do
+  it 'translate correctly' do
+    subject.translate("a1").should == [0, 0]
   end
 end
